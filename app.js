6c15dd307c566a297db9bd083fe53c0233cdda60
Bartosz Kowalczyk <bartoszkowalczyk@wp.pl>
2022-11-27 23:34:59 +0100

    download new data when scroll logic add

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ class InfiniteScroll {
 
     constructor(container) {
         this.container = container;
+        this.page = 1;
         this.init();
     }
 
     init() {
         window.onload = this.getData;
+
+        window.addEventListener('scroll', () => {
+            if(window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+                this.getData();
+            }
+        });
     }
 
     getData = async () => {
-        const apiUrl = `https://jsonplaceholder.typicode.com/posts?_page=1&_limit=4`;
+        const apiUrl = `https://jsonplaceholder.typicode.com/posts?_page=${this.page}&_limit=8`;
         try {
             const response = await fetch(apiUrl);
             const photosArray = await response.json();
@@ -18,17 +25,22 @@ class InfiniteScroll {
         } catch (error) {
             console.error(error);
         }
+        this.page++;
     }
 
     displayPosts(posts) {
         this.container.innerHTML += posts.map( post => {
             return `
                 <div class='post'>
-                    <h3>${post.title}</h3>
-                    <p>${post.body}</p>
+                    <h3>${this.capitalizeFirstLetter(post.title)}</h3>
+                    <p>${this.capitalizeFirstLetter(post.body)}</p>
                 </div>
             `
         }).join('');
+    }
+
+    capitalizeFirstLetter(text) {
+        return text.charAt(0).toUpperCase() + text.slice(1);
     }
 
 }
